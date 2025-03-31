234abb73242bc4a1547990b897c65917bbf2cbfe
serbina <serbina@net.chalmers.se>
2025-03-31 15:16:19 +0200

    Scan lexical errors fix

--- app/main.go
+++ app/main.go
@@ -89,7 +89,7 @@ func main() {
 
 			case NUMBER, DOLLAR, ATSIGN, CARET:
 				
-				fmt.Fprintf(os.Stderr, "[line %1] Error: Unexpected character: %c\n", ch)
+				fmt.Fprintf(os.Stderr, "[line 1] Error: Unexpected character: %ch", ch)
 				os.Exit(65)
 			}
 		}
