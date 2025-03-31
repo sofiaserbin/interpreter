f62ff1d6d02e89a3071f9858bd7143ebdad98d1a
serbina <serbina@net.chalmers.se>
2025-03-31 21:35:13 +0200

    Start with numerical literal

--- app/main.go
+++ app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"unicode"
 )
 
 func main() {
@@ -192,7 +193,18 @@ func main() {
 				has_error = true
 				indx++
 			default:
-				indx++
+				if (unicode.IsDigit(ch)){
+					var result []rune
+					for indx < len(fileContents) && fileContents[indx] != ' ' {
+						if (rune(fileContents[indx]) == '.' || unicode.IsDigit(rune(fileContents[index])))
+						result = append(result, rune(fileContents[indx]))
+						indx++
+					}
+					fmt.Printf("NUMBER %f %s", float(result), string(result))
+				} else{
+					indx++
+				}
+				
 			
 			}
 			}
